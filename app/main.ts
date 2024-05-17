c37d9ed595c96799e9a47aa1a51d16807d12917d
= <=>
2024-05-17 09:09:00 -0400

    pong

--- app/main.ts
+++ app/main.ts
@@ -3,8 +3,12 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+const server = net.createServer((connection: net.Socket) => {
+    connection.on("data", (buffer) => {
+        const message = buffer.toString();
+        console.log(message);
+        connection.write("+PONG\r\n");
+    })
 });
 
 server.listen(6379, "127.0.0.1");
